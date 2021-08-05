3dac2e222e5c044ff4dfee18cf04e86276d387f8
Tim Goff <timothy.d.goff@nasa.gov>
2021-08-05 10:25:33 -0400

    Add 2021 festivals

---
+++ data/frontporchfest_12.clj
@@ -0,0 +1,38 @@
+;; List of all bands (sanitized to remove anything extraneous, e.g. "Artist at Large")
+{:playlist-name "Front Porch Fest 12 Super List"
+ :artist-names [
+    "Rebirth Brass Band"
+    "Big Daddy Love"
+    "Jonathan Scales Fourchestra"
+    "The Judy Chops"
+    "Toubab Krewe"
+    "The Wooks"
+    "Josyln & The Sweet Compression"
+    "Striking Copper"
+    "The Nth Power"
+    "Fireside Collective"
+    "Tall Tall Trees"
+    "Music Road Co."
+    "Hambone Relay"
+    "Into The Fog"
+    ;"Slick Jr. & The Reactors"
+    ;; "Borrowed Earth"
+    ;; "Righteous Friendz Band"
+    ;; "Billy Woods"
+    "Marvelous Funkshun"
+    ;; "Abby Rasheed & The Sundry Collective"
+    ;"Gote"
+    ;; "Isaac Hadden Project"
+    ;; "Candelfirth"
+    ;; "Maggie Blankenship"
+    "Space Koi"
+    "The Freeway Jubilee"
+    ;Fluidity Performance Troupe
+    "Emma’s Lounge"
+    ;; "George Smith Band"
+    ;; "Maya Burgess"
+ ]
+ ; for any artists that are in spotify but not coming uop at the top of a name search
+ :finicky-artist-ids []
+
+ :songs-per-artist 5}
---
+++ data/roosterwalk_reunion.clj
@@ -0,0 +1,43 @@
+;; List of all bands (sanitized to remove anything extraneous, e.g. "Artist at Large")
+{:playlist-name "Rooster Walk Reunion 2021"
+ :artist-names [
+    "The Infamous Stringdusters"
+    "Robert Randolph Band"
+    "Big Something"
+    "Tauk"
+    "Pimps of Joytime"
+    "Yarn"
+    "Kendall Street Company"
+    "Jon Stickley Trio"
+    "DJ Williams' Shots Fired"
+    "Kat Wright"
+    "Violet Bell"
+    "Fireside Collective"
+    "Erin & the Wildfire"
+    "Josh Shilling" 
+    ;; "Wallace Mullinax"
+    "Sexbruise?"
+    "Big Daddy Love"
+    "South Hill Banks"
+    "Sanctum Sully"
+    "L Shape Lot"
+    ;; "Isaac Hadden Project"
+    "After Jack"
+    ;; "Jules & the Agreeables"
+    "Prosperity's Folly"
+    ;; "PHCC Jazz Band"
+]
+ ; for any artists that are in spotify but not coming uop at the top of a name search
+ :finicky-artist-ids [
+
+    ;; "Melvin Seals & JGB"
+    "35oDRMIqI8e2sWuTpq56Gz"
+
+    ;; "Larry Keel Experience"
+    "03F2tzTnr6K6S9c5aRP1ne"
+
+    ;; "Johnny Selman & Friends"
+    "0GPjCtyvaQ1VqWftvBPK0I"
+ ]
+
+ :songs-per-artist 5}
